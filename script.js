7547820f7dc85797a9dd01a7b6fa80f12ce19031
alvihossain1 <2020780@iub.edu.bd>
2023-11-11 23:42:53 +0600

    10th commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function submit() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var msg = document.getElementById("message").value;
+    var output;
+
+    if (name = "" || email == "" || msg == "") {
+        output = `
+        <div class="output"> 
+            <h3>Not All Fields are filled. Please fill up all the fields.</h3>
+        </div>
+        `;
+    }
+    else {
+        output = `
+        <div class="output">
+                        <h3>Record Submitted!</h3>
+                        <p>Thank You, ${name}</p>
+                        <p>Email: ${email}</p>
+                        <p>Message: ${msg}</p>
+                    </div>
+        `;
+    }
+
+
+    document.getElementById("generate").innerHTML = output;
+
+
+
+}
